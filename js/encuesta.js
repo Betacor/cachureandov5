807fa3321aeed13c09352939484626fe2896ddba
krynortz91 <kryn.ortz@gmail.com>
2023-02-02 13:20:05 -0300

    Intento de cambios encuesta

--- js/encuesta.js
+++ js/encuesta.js
@@ -1,9 +1,8 @@
 // mantiene ocultos el aviso de éxito
-$( document ).ready(function(){
-    $("#alerta-exito").hide();
-  });
-  
-  $("#btnRegistra").click(function(){
-     $("#alerta-exito").show("slow");
-  });
-  
+$(document).ready(function () {
+   $("#alerta-exito").hide();
+});
+
+$("#btnRegistra").click(function () {
+   $("#alerta-exito").show("slow");
+});
