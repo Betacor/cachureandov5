f382a60be49c91d13e879c17d0d77a1984d672fb
Sappy <macarenaramdohr@gmail.com>
2023-02-04 23:25:29 -0300

    grandes cambios

--- js/encuesta.js
+++ js/encuesta.js
@@ -1,9 +1,12 @@
 // mantiene ocultos el aviso de éxito
 $( document ).ready(function(){
-    $("#alerta-exito").hide();
+   $("#alerta-exito").hide();
+   $("input[type=radio]").click(function(){
+      $("#alerta-exito").show("slow");
+   });
+   $("#close-exito").click(function(){
+    $("#alerta-exito").hide("slow");
+   });
   });
-  
-  $("#btnRegistra").click(function(){
-     $("#alerta-exito").show("slow");
-  });
+
   
