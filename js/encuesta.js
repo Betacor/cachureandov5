a2ebc940a046edb312a60318935a6d20ca0cb874
maufuenbus <109995691+ylagosalfaro@users.noreply.github.com>
2023-02-04 23:05:26 -0300

    se agrega encuesta

--- js/encuesta.js
+++ js/encuesta.js
@@ -1,9 +1,26 @@
-// mantiene ocultos el aviso de éxito
-$( document ).ready(function(){
-    $("#alerta-exito").hide();
-  });
-  
-  $("#btnRegistra").click(function(){
-     $("#alerta-exito").show("slow");
-  });
-  
+jQuery(document).ready(function($){
+    
+	$(".btnrating").on('click',(function(e) {
+	
+	var previous_value = $("#selected_rating").val();
+	
+	var selected_value = $(this).attr("data-attr");
+	$("#selected_rating").val(selected_value);
+	
+	$(".selected-rating").empty();
+	$(".selected-rating").html(selected_value);
+	
+	for (i = 1; i <= selected_value; ++i) {
+	$("#rating-star-"+i).toggleClass('btn-warning');
+	$("#rating-star-"+i).toggleClass('btn-default');
+	}
+	
+	for (ix = 1; ix <= previous_value; ++ix) {
+	$("#rating-star-"+ix).toggleClass('btn-warning');
+	$("#rating-star-"+ix).toggleClass('btn-default');
+	}
+	
+	}));
+	
+		
+});
