ee6e867193b39c1f2e93dba14075955395d94e0c
Yu Lagos Alfaro <y.lagosalfaro@gmail.com>
2023-02-05 19:20:04 -0300

    deshabilito zoom imagen tops

--- js/tops.js
+++ js/tops.js
@@ -1,8 +1,9 @@
 // let imgTop = document.querySelector(".card-img-top");
-const cartas = document.querySelectorAll(".card");  
-const modalImg = document.getElementById("img-zoom");
-for(let i=0; i<cartas.length; i++){
-    cartas[i].addEventListener("click", (event)=>{
-        modalImg.src = event.target.attributes.src.value;
-    });
-}
+
+// const cartas = document.querySelectorAll(".card");  
+// const modalImg = document.getElementById("img-zoom");
+// for(let i=0; i<cartas.length; i++){
+//     cartas[i].addEventListener("click", (event)=>{
+//         modalImg.src = event.target.attributes.src.value;
+//     });
+// }
